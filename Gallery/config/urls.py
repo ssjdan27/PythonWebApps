17c97459c90e3def317873fa11360b6ccce95119
ssjdan27 <danielg272313@gmail.com>
2024-09-17 16:21:17 -0600

    project 3 start code

--- Gallery/config/urls.py
+++ Gallery/config/urls.py
@@ -1,7 +1,15 @@
+from django.views.generic import RedirectView
 from django.urls import path
-from photos.views import PhotoView, PhotoListView
+
+from photos.views import PhotoDetailView, PhotoListView
+
 
 urlpatterns = [
-    path('<str:name>', PhotoView.as_view()),
-    path('', PhotoListView.as_view()),
+
+    # Home
+    path('', RedirectView.as_view(url='photo/')),
+
+    # Photos
+    path('photo/', PhotoListView.as_view()),
+    path('photo/<int:id>', PhotoDetailView.as_view()),
 ]
--- Gallery/photos/views.py
+++ Gallery/photos/views.py
@@ -1,19 +1,29 @@
 from pathlib import Path
-from typing import Any
 from django.views.generic import TemplateView
 
-class PhotoView(TemplateView):
-    template_name = 'photo.html'
 
-    def get_context_data(self, **kwargs):
-        name = kwargs['name']
-        image = f'/static/images/{name}'
-        return {'photo': image}
+def photo_list():
+    def photo_details(i, f):
+        caption = f'Caption for Photo {i}' if i == 1 else None
+        return dict(id=i, file=f, caption=caption)
+
+    photos = Path('static/images').iterdir()
+    photos = [photo_details(i, f) for i, f in enumerate(photos)]
+    return photos
+
 
 class PhotoListView(TemplateView):
     template_name = 'photos.html'
 
     def get_context_data(self, **kwargs):
-        photos = Path('static/images').iterdir()
-        photos = [f for f in photos]
-        return dict(photos=photos)
+        return dict(photos=photo_list())
+
+
+class PhotoDetailView(TemplateView):
+    template_name = 'photo.html'
+
+    def get_context_data(self, **kwargs):
+        i = kwargs['id']
+        photos = photo_list()
+        p = photos[i]
+        return dict(photo=p)
