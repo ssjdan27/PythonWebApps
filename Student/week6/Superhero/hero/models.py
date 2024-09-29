75d5b22c310f0842047c6351fb923164a6878abe
Daniel Garza <danielg272313@gmail.com>
2024-09-29 11:26:29 -0600

    project 5 requirements

--- Student/week6/Superhero/hero/models.py
+++ Student/week6/Superhero/hero/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Superhero(models.Model):
+    name = models.CharField(max_length=100)
+    identity = models.CharField(max_length=100, default='')
+    description = models.TextField(default='')
+    strength = models.CharField(max_length=100, default='')
+    weakness = models.CharField(max_length=100, default='')
+    image = models.CharField(max_length=100, default='')
+
+    def __str__(self):
+        return self.name
